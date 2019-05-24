0a7401893f877140443b87ca77250188e470e2d0
Elaine Lee <elaine.lee@ff0000.com>
2019-05-23 18:22:33 -0700

    add react router single Launch display button

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./logo.jpg";
 import "./App.css";
 import Launches from "./components/Launches";
+import Launch from "./components/Launch";
 
 //creating our client
 const client = new ApolloClient({
@@ -14,10 +16,13 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="container">
-          <img src={logo} alt="SpaceX" style={{ width: 300, display: "block", margin: "auto" }} />
-          <Launches />
-        </div>
+        <Router>
+          <div className="container">
+            <img src={logo} alt="SpaceX" style={{ width: 300, display: "block", margin: "auto" }} />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
